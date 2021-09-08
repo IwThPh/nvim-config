9d9c065590d69dce41caaf51bf040d53d317647f
Iwan Phillips <iwan.phillips@worthers.com>
2021-09-08 22:46:55 +0100

    Fix: changed telescope highlights for better contrast.

--- lua/plugin/telescope/init.lua
+++ lua/plugin/telescope/init.lua
@@ -98,15 +98,14 @@ require('telescope').load_extension('dap')
 
 -- Highlight groups.
 local nvim_cmd = vim.api.nvim_command
-nvim_cmd [[ hi def link TelescopeSelection vimWarn ]]
+nvim_cmd [[ hi def link TelescopeSelection LspDiagnosticsVirtualTextWarning ]]
 nvim_cmd [[ hi def link TelescopeSelectionCaret Keyword ]]
 nvim_cmd [[ hi def link TelescopeMultiSelection Normal ]]
 nvim_cmd [[ hi def link TelescopeNormal TelescopeNormal ]]
 nvim_cmd [[ hi def link TelescopeBorder CursorLineNr ]]
-nvim_cmd [[ hi def link TelescopePromptBorder Todo ]]
 nvim_cmd [[ hi def link TelescopeResultsBorder Normal ]]
 nvim_cmd [[ hi def link TelescopePreviewBorder Normal ]]
-nvim_cmd [[ hi def link TelescopeMatching IncSearch ]]
+nvim_cmd [[ hi def link TelescopeMatching LspDiagnosticsWarning ]]
 nvim_cmd [[ hi def link TelescopePromptPrefix Keyword ]]
 
 -- Initially taken from TJDevris' dots. github.com/tjdevries/config_manager
