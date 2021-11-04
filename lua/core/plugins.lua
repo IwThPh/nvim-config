f7c03d15b92041982d6fa2e7354c0de955725252
Iwan Phillips <iwan.phillips@worthers.com>
2021-11-04 23:14:29 +0000

    Added: windline.nvim

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -19,6 +19,12 @@ return require("packer").startup({
 
 		-- Visual / UI
 		use({
+			"windwp/windline.nvim",
+			config = function ()
+				require('wlsample.bubble2')
+			end
+		})
+		use({
 			"folke/persistence.nvim",
 			event = "BufReadPre", -- this will only start session saving when an actual file was opened
 			module = "persistence",
