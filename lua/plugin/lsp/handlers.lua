07bb52618963177e761b8e1479fd7a4c6c7f9ff2
Iwan Phillips <iwan.phillips@worthers.com>
2021-11-04 23:32:22 +0000

    Removed: unused lsp handlers.

--- lua/plugin/lsp/handlers.lua
+++ lua/plugin/lsp/handlers.lua
@@ -9,19 +9,6 @@ local border = {
 	{"│", "FloatBorder"},
 }
 
---Custom LSP Handlers
-vim.lsp.handlers["textDocument/definition"] = function(_, _, result)
-  if not result or vim.tbl_isempty(result) then
-    print("[LSP] Could not find definition")
-    return
-  end
-
-  if vim.tbl_islist(result) then
-    vim.lsp.util.jump_to_location(result[1])
-  else
-    vim.lsp.util.jump_to_location(result)
-  end
-end
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   virtual_text = true,
