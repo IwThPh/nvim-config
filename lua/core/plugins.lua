1256ff50fedc4d39c081b13b5299856885ee2b56
Iwan Phillips <iwan.phillips@worthers.com>
2021-11-04 23:12:51 +0000

    Luafmt

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,35 +1,23 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-  execute 'packadd packer.nvim'
+	execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+	execute("packadd packer.nvim")
 end
 
-return require('packer').startup {
+return require("packer").startup({
 	function(use)
 		-- Packer can manage itself as an optional plugin
-		use 'wbthomason/packer.nvim'
+		use("wbthomason/packer.nvim")
 
 		-- Deps
-		use 'nvim-lua/plenary.nvim'
-		use 'nvim-lua/popup.nvim'
+		use("nvim-lua/plenary.nvim")
+		use("nvim-lua/popup.nvim")
 
 		-- Visual / UI
-		use 'romgrk/barbar.nvim' 					-- Buffer line
-		use 'norcalli/nvim-colorizer.lua' 			-- Hex Colorizer
-		use {										-- File explorer
-			'kyazdani42/nvim-tree.lua',
-			requires = {'kyazdani42/nvim-web-devicons'}
-		}
-		use {
-			'nacro90/numb.nvim',					-- Line viewer E.g. {:number}
-			config = function() require('numb').setup() end,
-		}
-		use "lukas-reineke/indent-blankline.nvim"
-		use {
 		use({
 			"folke/persistence.nvim",
 			event = "BufReadPre", -- this will only start session saving when an actual file was opened
@@ -38,42 +26,61 @@ return require('packer').startup {
 				require("persistence").setup()
 			end,
 		})
+		use("romgrk/barbar.nvim") -- Buffer line
+		use("norcalli/nvim-colorizer.lua") -- Hex Colorizer
+		use({ -- File explorer
+			"kyazdani42/nvim-tree.lua",
+			requires = { "kyazdani42/nvim-web-devicons" },
+		})
 		use("folke/zen-mode.nvim")
 		use("folke/twilight.nvim")
+		use({
+			"nacro90/numb.nvim", -- Line viewer E.g. {:number}
+			config = function()
+				require("numb").setup()
+			end,
+		})
+		use("lukas-reineke/indent-blankline.nvim")
+		use({
 			 "vuki656/package-info.nvim",					-- Line viewer E.g. {:number}
-			config = function() require('package-info').setup() end,
-		}
-		use 'gelguy/wilder.nvim'
+			requires = "MunifTanjim/nui.nvim",
+			config = function()
+				require("package-info").setup()
+			end,
+		})
+		use("gelguy/wilder.nvim")
 
 		-- Quick Formatting/Editing
-		use {
-			'windwp/nvim-autopairs', 				-- Auto pairs for '(' '[' '{'
-			config = function() require('nvim-autopairs').setup() end,
-		}
-		use 'tpope/vim-surround' 					-- Surround selection
-		use 'tpope/vim-commentary' 					-- Commenting out lines easily
+		use({
+			"windwp/nvim-autopairs", -- Auto pairs for '(' '[' '{'
+			config = function()
+				require("nvim-autopairs").setup()
+			end,
+		})
+		use("tpope/vim-surround") -- Surround selection
+		use("tpope/vim-commentary") -- Commenting out lines easily
 
 		-- Treesitter
-		use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+		use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
 
 		-- Fuzzy Finder
-		use {										-- Extensible Fuzzy Finder
-			'nvim-telescope/telescope.nvim',
+		use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+		use({ -- Extensible Fuzzy Finder
+			"nvim-telescope/telescope.nvim",
 			require = {
-				{ 'nvim-lua/plenary.nvim' },
-				{ 'nvim-lua/popup.nvim' },
-			}
-		}
-		use 'nvim-telescope/telescope-fzf-writer.nvim'
-		use 'nvim-telescope/telescope-dap.nvim'
+				{ "nvim-lua/plenary.nvim" },
+				{ "nvim-lua/popup.nvim" },
+			},
+		})
+		use("nvim-telescope/telescope-dap.nvim")
 
 		-- Git
-		use 'tpope/vim-fugitive'
-		use {
-			'lewis6991/gitsigns.nvim',
-			require = { 'nvim-lua/plenary.nvim' }
-		}
-		use 'sindrets/diffview.nvim'
+		use("tpope/vim-fugitive")
+		use({
+			"lewis6991/gitsigns.nvim",
+			require = { "nvim-lua/plenary.nvim" },
+		})
+		use("sindrets/diffview.nvim")
 
 		-- Neovim LSP Config
 		use 'neovim/nvim-lspconfig' 				-- Official LSP quick setup
@@ -87,13 +94,14 @@ return require('packer').startup {
 				{'hrsh7th/cmp-buffer'},				-- Install the buffer completion source
 			}
 		}
-		use 'norcalli/snippets.nvim'				-- Snippets
 		use 'kosayoda/nvim-lightbulb' 				-- VSCode like light bulb for code actions
-		use 'onsails/lspkind-nvim' 					-- Completion icons.
 		use {
+		use("norcalli/snippets.nvim") -- Snippets
+		use("onsails/lspkind-nvim") -- Completion icons.
+		use({
 			"folke/lsp-trouble.nvim",				-- LSP Diagnostics list
 			requires = "kyazdani42/nvim-web-devicons",
-		}
+		})
 		use({
 			"folke/todo-comments.nvim",
 			requires = "nvim-lua/plenary.nvim",
@@ -110,20 +118,19 @@ return require('packer').startup {
 		use("simrat39/symbols-outline.nvim") -- Symbol outline tree viewer (Like vista)
 
 		-- Start up time profilling
-		use 'dstein64/vim-startuptime'
+		use("dstein64/vim-startuptime")
 
 		-- Project rooting
-		use 'airblade/vim-rooter'
+		use("airblade/vim-rooter")
 
 		-- Debug/Testing
-		use 'vim-test/vim-test'						-- Test runner
-		use 'mfussenegger/nvim-dap'					-- Debug Adapter Protocol
+		use("vim-test/vim-test") -- Test runner
+		use("mfussenegger/nvim-dap") -- Debug Adapter Protocol
 
 		-- Colourschemes
-		use 'marko-cerovac/material.nvim'
-		use 'folke/tokyonight.nvim'
-		use 'EdenEast/nightfox.nvim'
-
+		use("marko-cerovac/material.nvim")
+		use("folke/tokyonight.nvim")
+		use("EdenEast/nightfox.nvim")
 	end,
 	config = {
 		_display = {
@@ -131,11 +138,11 @@ return require('packer').startup {
 				-- Can only use plenary when we have our plugins.
 				--  We can only get plenary when we don't have our plugins ;)
 				local ok, float_win = pcall(function()
-					return require('plenary.window.float').percentage_range_window(0.8, 0.8)
+					return require("plenary.window.float").percentage_range_window(0.8, 0.8)
 				end)
 
 				if not ok then
-					vim.cmd [[65vnew  [packer] ]]
+					vim.cmd([[65vnew  [packer] ]])
 
 					return vim.api.nvim_get_current_win(), vim.api.nvim_get_current_buf()
 				end
@@ -144,10 +151,10 @@ return require('packer').startup {
 				local win = float_win.win_id
 
 				vim.api.nvim_buf_set_name(bufnr, name)
-				vim.api.nvim_win_set_option(win, 'winblend', 10)
+				vim.api.nvim_win_set_option(win, "winblend", 10)
 
 				return win, bufnr
-			end
+			end,
 		},
-	}
-}
+		},
+})
