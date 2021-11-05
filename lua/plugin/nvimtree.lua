b254dcf4d1adfe8e18b99922e3f2ae9ea7fddc4a
Iwan Phillips <iwan.phillips@worthers.com>
2021-11-05 01:58:24 +0000

    Fix: updated nvim-tree lsp diagnostics config.

--- lua/plugin/nvimtree.lua
+++ lua/plugin/nvimtree.lua
@@ -1,90 +1,98 @@
 vim.g.nvim_tree_indent_markers = 1 --0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_git_hl = 1 --0 by default, will enable file highlight for git attributes (can be used without the icons).
-vim.g.nvim_tree_root_folder_modifier = ':~' --This is the default. See :help filename-modifiers for more options
+vim.g.nvim_tree_root_folder_modifier = ":~" --This is the default. See :help filename-modifiers for more options
 vim.g.nvim_tree_add_trailing = 1 --0 by default, append a trailing slash to folder names
 vim.g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1 }
 
 vim.g.nvim_tree_icons = {
-	default= '',
-	symlink= '',
-	git= {
-		unstaged= "",
-		staged= "",
-		unmerged= "",
-		renamed= "",
-		untracked= "",
-		deleted= "",
-		ignored= "◌"
+	default = "",
+	symlink = "",
+	git = {
+		unstaged = "",
+		staged = "",
+		unmerged = "",
+		renamed = "",
+		untracked = "",
+		deleted = "",
+		ignored = "◌",
 	},
-	folder= {
-		default= "",
-		open= "",
-		empty= "",
-		empty_open= "",
-		symlink= "",
+	folder = {
+		default = "",
+		open = "",
+		empty = "",
+		empty_open = "",
+		symlink = "",
 	},
-	lsp= {
+	lsp = {
 		error = "",
 		warning = "",
 		hint = "",
-		info= ""
-	}
+		info = "",
+	},
 }
 
 -- following options are the default
-require'nvim-tree'.setup {
-  -- disables netrw completely
-  disable_netrw       = true,
-  -- hijack netrw window on startup
-  hijack_netrw        = true,
-  -- open the tree when running this setup function
-  open_on_setup       = true,
-  -- will not open on setup if the filetype is in this list
-  ignore_ft_on_setup  = { 'startify '},
-  -- closes neovim automatically when the tree is the last **WINDOW** in the view
-  auto_close          = false,
-  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-  open_on_tab         = false,
-  -- hijack the cursor in the tree to put it at the start of the filename
-  hijack_cursor       = false,
-  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-  update_cwd          = false,
-  -- show lsp diagnostics in the signcolumn
-  lsp_diagnostics     = true,
-  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-  update_focused_file = {
-    -- enables the feature
-    enable      = true,
-    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-    -- only relevant when `update_focused_file.enable` is true
-    update_cwd  = false,
-    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-    ignore_list = { '.git', '.cache' }
-  },
-  -- configuration options for the system open command (`s` in the tree by default)
-  system_open = {
-    -- the command to run this, leaving nil should work in most cases
-    cmd  = nil,
-    -- the command arguments as a list
-    args = {}
-  },
+require("nvim-tree").setup({
+	-- disables netrw completely
+	disable_netrw = true,
+	-- hijack netrw window on startup
+	hijack_netrw = true,
+	-- open the tree when running this setup function
+	open_on_setup = true,
+	-- will not open on setup if the filetype is in this list
+	ignore_ft_on_setup = { "startify " },
+	-- closes neovim automatically when the tree is the last **WINDOW** in the view
+	auto_close = false,
+	-- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+	open_on_tab = false,
+	-- hijack the cursor in the tree to put it at the start of the filename
+	hijack_cursor = false,
+	-- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
+	update_cwd = false,
+	-- show lsp diagnostics in the signcolumn
+	diagnostics = {
+		enable = false,
+		icons = {
+			error = "",
+			warning = "",
+			hint = "",
+			info = "",
+		},
+	},
+	-- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+	update_focused_file = {
+		-- enables the feature
+		enable = true,
+		-- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+		-- only relevant when `update_focused_file.enable` is true
+		update_cwd = false,
+		-- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+		-- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+		ignore_list = { ".git", ".cache" },
+	},
+	-- configuration options for the system open command (`s` in the tree by default)
+	system_open = {
+		-- the command to run this, leaving nil should work in most cases
+		cmd = nil,
+		-- the command arguments as a list
+		args = {},
+	},
 
-  view = {
-    -- width of the window, can be either a number (columns) or a string in `%`
-    width = 50,
-    -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
-    side = 'left',
-    -- if true the tree will resize itself after opening a file
-    auto_resize = true,
-    mappings = {
-      -- custom only false will merge the list with the default mappings
-      -- if true, it will only use your list to set the mappings
-      custom_only = false,
-      -- list of mappings to set on the tree manually
-      list = {}
-    }
-  }
-}
+	view = {
+		-- width of the window, can be either a number (columns) or a string in `%`
+		width = 50,
+		-- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+		side = "left",
+		-- if true the tree will resize itself after opening a file
+		auto_resize = true,
+		mappings = {
+			-- custom only false will merge the list with the default mappings
+			-- if true, it will only use your list to set the mappings
+			custom_only = false,
+			-- list of mappings to set on the tree manually
+			list = {},
+		},
+	},
+})
 
-vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>f", "<cmd>NvimTreeToggle<CR>", { noremap = true, silent = true })
