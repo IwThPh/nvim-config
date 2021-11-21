69f7e429b9945c94b15557135858200c07822a14
Iwan Phillips <iwan.phillips@worthers.com>
2021-11-21 23:57:51 +0000

    Updated: tree-sitter indents, with improved folding.
    vim.o.foldtext = [[substitute(getline(v:foldstart),'\t',repeat(' ',&tabstop),'g').'···'.trim(getline(v:foldend))]]
    
    This was taken from a reddit post.
    Makes the tree-sitter folds render nicely.

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,10 +1,17 @@
-require'nvim-treesitter'.setup{
-  	ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-	highlight = { 
-		enable = true 
+require("nvim-treesitter.configs").setup({
+	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	highlight = {
+		enable = true,
 	},
-}
---TreeSitter based code folding
-vim.api.nvim_command('set foldmethod=expr')
-vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
+	indent = {
+		enable = true,
+	},
+})
 
+--TreeSitter based code folding
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+
+vim.o.foldtext = [[substitute(getline(v:foldstart),'\t',repeat(' ',&tabstop),'g').'···'.trim(getline(v:foldend))]]
+vim.o.foldnestmax = 3
+vim.o.foldminlines = 1
