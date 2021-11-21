beb5d5e12c9b78b46fa66d54d9f5bc448948cb91
Iwan Phillips <iwan.phillips@worthers.com>
2021-11-21 23:55:48 +0000

    Updated: commented out currently unused null-ls builtins.
    Currently trialing these when time available.

--- lua/plugin/lsp/null-ls.lua
+++ lua/plugin/lsp/null-ls.lua
@@ -5,8 +5,15 @@ null_ls.config({
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.sqlformat,
 		null_ls.builtins.formatting.eslint_d,
-		null_ls.builtins.formatting.prettier,
+		-- null_ls.builtins.formatting.prettier,
+		-- null_ls.builtins.formatting.fixjson,
+
 		null_ls.builtins.diagnostics.write_good,
-		null_ls.builtins.code_actions.gitsigns,
+
+		-- null_ls.builtins.diagnostics.phpstan,
+		-- null_ls.builtins.diagnostics.psalm,
+		-- null_ls.builtins.diagnostics.phpcs,
+
+		-- null_ls.builtins.code_actions.gitsigns,
 	},
 })
