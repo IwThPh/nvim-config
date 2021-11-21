655d75491965b1334432c874cbd25ec3ae61738b
Iwan Phillips <iwan.phillips@worthers.com>
2021-11-21 23:54:09 +0000

    Updated: true-zen config to utilise kitty integration.

--- lua/plugin/truezen.lua
+++ lua/plugin/truezen.lua
@@ -29,7 +29,7 @@ zen.setup({
       ruler = false, -- disables the ruler text in the cmd line area
       showcmd = false, -- disables the command in the last line of the screen
     },
-    twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+    twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
     gitsigns = { enabled = false }, -- disables git signs
     tmux = { enabled = false }, -- disables the tmux statusline
     -- this will change the font size on kitty when in zen mode
@@ -37,7 +37,7 @@ zen.setup({
     -- - allow_remote_control socket-only
     -- - listen_on unix:/tmp/kitty
     kitty = {
-      enabled = false,
+      enabled = true,
       font = "+4", -- font size increment
     },
   },
