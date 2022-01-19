0cbdb6f0138be2ccea7bf94159ffe036e3583154
Iwan Phillips <iwan.phillips@worthers.com>
2022-01-19 22:12:13 +0000

    Updated LSP sign definitions.

--- lua/plugin/lsp/handlers.lua
+++ lua/plugin/lsp/handlers.lua
@@ -20,8 +20,8 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagn
 vim.lsp.handlers["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border})
 vim.lsp.handlers["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border})
 
-vim.fn.sign_define("LspDiagnosticsSignError", { text="" })
-vim.fn.sign_define("LspDiagnosticsSignWarning", { text="" })
-vim.fn.sign_define("LspDiagnosticsSignInformation", { text="" })
-vim.fn.sign_define("LspDiagnosticsSignHint", { text="" })
+vim.fn.sign_define("DiagnosticSignError", { text="" })
+vim.fn.sign_define("DiagnosticSignWarning", { text="" })
+vim.fn.sign_define("DiagnosticSignInformation", { text="" })
+vim.fn.sign_define("DiagnosticSignHint", { text="" })
 
