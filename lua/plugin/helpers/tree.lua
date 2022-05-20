755d6057f7912bd989d500c16faa4ed956cd6002
Iwan Phillips <iwan.phillips@worthers.com>
2022-05-21 00:11:36 +0100

    feat(tree): added percentage based width, 20%.

--- lua/plugin/helpers/tree.lua
+++ lua/plugin/helpers/tree.lua
@@ -3,9 +3,11 @@ local tree = {}
 
 tree.isOpen = false
 
+
 tree.open = function()
+	local width = vim.api.nvim_get_option("columns")
 	tree.isOpen = true
-	barbarstate.set_offset(31, "FileTree")
+	barbarstate.set_offset(math.ceil(width * 1 / 5), "NeoTree")
 	vim.cmd [[ Neotree reveal ]]
 end
 
--- lua/plugin/neotree.lua
+++ lua/plugin/neotree.lua
@@ -72,7 +72,7 @@ require("neo-tree").setup({
 	},
 	window = {
 		position = "left",
-		width = 30,
+		width = '20%',
 		mapping_options = {
 			noremap = true,
 			nowait = true,
