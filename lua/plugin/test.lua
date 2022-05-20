c8bea0f4190b24fedc0eaf8467313d55a83d1e3e
Iwan Phillips <iwan.phillips@worthers.com>
2022-05-21 00:10:18 +0100

    feat: updated test runner to use smaller window.

--- lua/plugin/test.lua
+++ lua/plugin/test.lua
@@ -8,6 +8,7 @@ set_keymap('n', 'ta', ':TestSuite<CR>', opts)
 set_keymap('n', 'tt', ':TestLast<CR>', opts)
 
 vim.api.nvim_set_var("test#strategy", "neovim")
+vim.api.nvim_set_var("test#neovim#term_position", "botright 10")
 vim.api.nvim_set_var("test#php#runner", "phpunit")
 vim.api.nvim_set_var("test#php#phpunit#executable", "./vendor/bin/phpunit")
 
