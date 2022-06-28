e87bc9000209295b63dd3030c1c512d281aa62bc
Iwan Phillips <iwanthomphil@gmail.com>
2022-06-29 00:36:46 +0100

    --wip-- [skip ci]

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,22 +20,7 @@ local plugins = {
 
 	["EdenEast/nightfox.nvim"] = {
 		config = function()
-			require("nightfox").setup({
-				options = {
-					transparent = false,
-					terminal_color = true,
-					styles = {
-						comments = "italic", -- change style of comments to be italic
-						keywords = "bold", -- change style of keywords to be bold
-						functions = "italic,bold", -- styles can be a comma separated list
-					},
-					inverse = {
-						match_paren = true, -- Enable/Disable inverse highlighting for match parens
-						visual = true, -- Enable/Disable inverse highlighting for visual selection
-						search = true, -- Enable/Disable inverse highlights for search highlights
-					},
-				},
-			})
+			require("plugins.configs.nightfox")
 		end,
 	},
 
