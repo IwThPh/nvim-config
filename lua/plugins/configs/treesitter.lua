14961a23ef80f5a1993b36cce0e10b28efbd42e5
Iwan Phillips <iwanthomphil@gmail.com>
2022-06-30 00:14:11 +0100

    --wip-- [skip ci]

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -6,16 +6,27 @@ end
 
 local options = {
    ensure_installed = {
-      "lua",
-      "vim",
+      "all",
    },
    highlight = {
       enable = true,
       use_languagetree = true,
    },
+	indent = {
+		enable = true,
+	},
 }
 
 -- check for any override
 options = require("core.utils").load_override(options, "nvim-treesitter/nvim-treesitter")
+
+local o = vim.o
+
+o.foldmethod = "expr"
+o.foldexpr = "v:lnum==1?'>1':getline(v:lnum)=~'use'?1:nvim_treesitter#foldexpr()"
+o.foldtext = [[substitute(getline(v:foldstart),'\t',repeat(' ',&tabstop),'g').'···'.trim(getline(v:foldend))]]
+o.foldnestmax = 3
+o.foldminlines = 1
+o.foldlevelstart=1
 
 treesitter.setup(options)
