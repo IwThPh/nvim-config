ebab78090a531e631675596d4d487b29ae08188c
Iwan Phillips <iwan.phillips@huboo.com>
2022-11-20 14:11:04 +0000

    --wip-- [skip ci]

--- lua/plugins/configs/null-ls.lua
+++ lua/plugins/configs/null-ls.lua
@@ -21,13 +21,13 @@ null_ls.setup({
 		null_ls.builtins.formatting.phpcsfixer,
 
 		-- null_ls.builtins.diagnostics.write_good,
-		null_ls.builtins.diagnostics.phpstan.with({
-			command = "docker",
-			args = {
-				"run", "-v", "$PWD:/app", "--rm", "ghcr.io/phpstan/phpstan", "analyze", "--error-format", "json", "--level", "1", "--no-progress", "$FILENAME",
-			},
-			method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
-		}),
+		-- null_ls.builtins.diagnostics.phpstan.with({
+		-- 	command = "docker",
+		-- 	args = {
+		-- 		"run", "-v", "$PWD:/app", "--rm", "ghcr.io/phpstan/phpstan", "analyze", "--error-format", "json", "--level", "1", "--no-progress", "$FILENAME",
+		-- 	},
+		-- 	method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+		-- }),
 		-- null_ls.builtins.diagnostics.psalm,
 
 		-- null_ls.builtins.code_actions.gitsigns,
--- lua/ui/lsp.lua
+++ lua/ui/lsp.lua
@@ -1,6 +1,17 @@
+local border = {
+	{ "╭", "FloatBorder" },
+	{ "─", "FloatBorder" },
+	{ "╮", "FloatBorder" },
+	{ "│", "FloatBorder" },
+	{ "╯", "FloatBorder" },
+	{ "─", "FloatBorder" },
+	{ "╰", "FloatBorder" },
+	{ "│", "FloatBorder" },
+}
+
 local function lspSymbol(name, icon)
-   local hl = "DiagnosticSign" .. name
-   vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
+	local hl = "DiagnosticSign" .. name
+	vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
 end
 
 lspSymbol("Error", "")
@@ -8,40 +19,57 @@ lspSymbol("Info", "")
 lspSymbol("Hint", "")
 lspSymbol("Warn", "")
 
-vim.diagnostic.config {
-   virtual_text = {
-      prefix = "",
-   },
-   signs = true,
-   underline = true,
-   update_in_insert = false,
-}
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "",
+		source = true,
+	},
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+})
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-   border = "single",
+local function markdownHover(_, result, ctx, config)
+	config = config or {}
+	config.focus_id = ctx.method
+	if not (result and result.contents) then
+		return
+	end
+	local markdown_lines = util.convert_input_to_markdown_lines(result.contents, {})
+	markdown_lines = util.trim_empty_lines(markdown_lines)
+	if vim.tbl_isempty(markdown_lines) then
+		return
+	end
+	return util.open_floating_preview(markdown_lines, "markdown", config)
+end
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(markdownHover, {
+	border = "single",
+	-- border = border,
 })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-   border = "single",
+	border = "single",
+	-- border = border,
 })
 
 -- suppress error messages from lang servers
 vim.notify = function(msg, log_level)
-   if msg:match "exit code" then
-      return
-   end
-   if log_level == vim.log.levels.ERROR then
-      vim.api.nvim_err_writeln(msg)
-   else
-      vim.api.nvim_echo({ { msg } }, true, {})
-   end
+	if msg:match("exit code") then
+		return
+	end
+	if log_level == vim.log.levels.ERROR then
+		vim.api.nvim_err_writeln(msg)
+	else
+		vim.api.nvim_echo({ { msg } }, true, {})
+	end
 end
 
 -- Borders for LspInfo winodw
-local win = require "lspconfig.ui.windows"
+local win = require("lspconfig.ui.windows")
 local _default_opts = win.default_opts
 
 win.default_opts = function(options)
-   local opts = _default_opts(options)
-   opts.border = "single"
-   return opts
+	local opts = _default_opts(options)
+	opts.border = "single"
+	return opts
 end
