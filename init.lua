7007cbcc84d1f8719e38b98afc4ffb7b07bf029d
Iwan Phillips <iwan.phillips@huboo.com>
2023-06-21 08:39:05 +0100

    --wip-- [skip ci]

--- init.lua
+++ init.lua
@@ -10,3 +10,5 @@
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+
---
+++ lua/plugins/chatgpt.lua
@@ -0,0 +1,14 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("chatgpt").setup({
+      api_key_cmd = "more /Users/iwanp/.config/openai/key",
+    })
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+}
