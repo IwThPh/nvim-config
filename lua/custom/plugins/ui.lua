c43f04ba2f13bd8d19e483b1b79d3d5ee8b7a803
Iwan Phillips <iwan.phillips@huboo.com>
2024-02-21 14:40:31 +0000

    --wip-- [skip ci]

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -1,179 +1,166 @@
 local Util = require("custom.util")
 return {
-  { -- Better `vim.notify()`
-    "rcarriga/nvim-notify",
-    keys = {
-      {
-        "<leader>un",
-        function() require("notify").dismiss({ silent = true, pending = true }) end,
-        desc = "Dismiss all Notifications",
-      },
-    },
-    opts = {
-      timeout = 3000,
-      level = vim.log.levels.WARN,
-      render = "compact",
-      stages = "fade",
-      max_height = function() return math.floor(vim.o.lines * 0.5) end,
-      max_width = function() return math.floor(vim.o.columns * 0.5) end,
-
-      on_open = function(win)
-        local config = vim.api.nvim_win_get_config(win)
-        config.border = "single"
-        vim.api.nvim_win_set_config(win, config)
-      end
-    },
-    init = function() vim.notify = require("notify") end,
-  },
-
-  { -- better vim.ui
-    "stevearc/dressing.nvim",
-    lazy = true,
-    config = function()
-      require("dressing").setup({
-        input = {
-          border = "solid",
-        },
-        select = {
-          border = "solid",
-          telescope = Util.telescope.theme({
-            layout_config = {
-              height = 0.25,
-            },
-          }),
-        },
-      })
-    end,
-    init = function()
-      vim.ui.select = function(...)
-        require("lazy").load({ plugins = { "dressing.nvim" } })
-        return vim.ui.select(...)
-      end
-      vim.ui.input = function(...)
-        require("lazy").load({ plugins = { "dressing.nvim" } })
-        return vim.ui.input(...)
-      end
-    end,
-  },
-
-  { -- statusline
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function()
-      local icons = require("custom.util.icons")
-
-      return {
-        options = {
-          theme = "auto",
-          globalstatus = true,
-          -- component_separators = { left = '', right = '' },
-          -- section_separators = { left = '', right = '' },
-          component_separators = '',
-          section_separators = '',
-          disabled_filetypes = { statusline = { "dashboard", "alpha" } },
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch" },
-          lualine_c = {
+    { -- Better `vim.notify()`
+        "rcarriga/nvim-notify",
+        keys = {
             {
-              "diagnostics",
-              symbols = {
-                error = icons.diagnostics.Error,
-                warn = icons.diagnostics.Warn,
-                info = icons.diagnostics.Info,
-                hint = icons.diagnostics.Hint,
-              },
+                "<leader>un",
+                function()
+                    require("notify").dismiss({ silent = true, pending = true })
+                end,
+                desc = "Dismiss all Notifications",
             },
-            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-            { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
-            -- stylua: ignore
-            {
-              function() return require("nvim-navic").get_location() end,
-              cond = function() return package.loaded["nvim-navic"] and require("nvim-navic").is_available() end,
-            },
-          },
-          lualine_x = {
+        },
+        opts = {
+            timeout = 3000,
+            level = vim.log.levels.WARN,
+            render = "compact",
+            stages = "fade",
+            max_height = function()
+                return math.floor(vim.o.lines * 0.5)
+            end,
+            max_width = function()
+                return math.floor(vim.o.columns * 0.5)
+            end,
+
+            on_open = function(win)
+                local config = vim.api.nvim_win_get_config(win)
+                config.border = "single"
+                vim.api.nvim_win_set_config(win, config)
+            end,
+        },
+        init = function()
+            vim.notify = require("notify")
+        end,
+    },
+
+    { -- better vim.ui
+        "stevearc/dressing.nvim",
+        lazy = true,
+        config = function()
+            require("dressing").setup({
+                input = {
+                    border = "solid",
+                },
+                select = {
+                    border = "solid",
+                    telescope = Util.telescope.theme({
+                        layout_config = {
+                            height = 0.25,
+                        },
+                    }),
+                },
+            })
+        end,
+        init = function()
+            vim.ui.select = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.select(...)
+            end
+            vim.ui.input = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.input(...)
+            end
+        end,
+    },
+
+    { -- statusline
+        "nvim-lualine/lualine.nvim",
+        event = "VeryLazy",
+        opts = function()
+            local icons = require("custom.util.icons")
+
+            return {
+                options = {
+                    theme = "auto",
+                    globalstatus = true,
+                    -- component_separators = { left = '', right = '' },
+                    -- section_separators = { left = '', right = '' },
+                    component_separators = "",
+                    section_separators = "",
+                    disabled_filetypes = { statusline = { "dashboard", "alpha" } },
+                },
+                sections = {
+                    lualine_a = { "mode" },
+                    lualine_b = { "branch" },
+                    lualine_c = {
+                        {
+                            "diagnostics",
+                            symbols = {
+                                error = icons.diagnostics.Error,
+                                warn = icons.diagnostics.Warn,
+                                info = icons.diagnostics.Info,
+                                hint = icons.diagnostics.Hint,
+                            },
+                        },
+                        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+                        { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+                        -- stylua: ignore
+                    },
+                    lualine_x = {
             -- stylua: ignore
             {
               function() return "  " .. require("dap").status() end,
               cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
               color = Util.ui.fg("Debug"),
             },
-            { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = Util.ui.fg("Special") },
-            {
-              "diff",
-              symbols = {
-                added = icons.git.added,
-                modified = icons.git.modified,
-                removed = icons.git.removed,
-              },
-            },
-          },
-          lualine_y = {
-            { "progress", separator = " ",                  padding = { left = 1, right = 0 } },
-            { "location", padding = { left = 0, right = 1 } },
-          },
-          lualine_z = {
-            -- function() return " " .. os.date("%R") end,
-          },
-        },
-        extensions = { "lazy" },
-      }
-    end,
-  },
-
-  { -- indent guides for Neovim
-    "lukas-reineke/indent-blankline.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    main = "ibl",
-    opts = {
-      indent = {
-        char = "│",
-      },
-      exclude = {
-        filetypes = {
-          "help",
-          "alpha",
-          "dashboard",
-          "neo-tree",
-          "Trouble",
-          "lazy",
-          "mason",
-          "notify",
-          "toggleterm",
-          "lazyterm",
-        },
-      },
-      scope = { enabled = false },
+                        {
+                            require("lazy.status").updates,
+                            cond = require("lazy.status").has_updates,
+                            color = Util.ui.fg("Special"),
+                        },
+                        {
+                            "diff",
+                            symbols = {
+                                added = icons.git.added,
+                                modified = icons.git.modified,
+                                removed = icons.git.removed,
+                            },
+                        },
+                    },
+                    lualine_y = {
+                        { "progress", separator = " ", padding = { left = 1, right = 0 } },
+                        { "location", padding = { left = 0, right = 1 } },
+                    },
+                    lualine_z = {
+                        -- function() return " " .. os.date("%R") end,
+                    },
+                },
+                extensions = { "lazy" },
+            }
+        end,
     },
-  },
 
-  { "folke/which-key.nvim",        opts = {} },
+    { -- indent guides for Neovim
+        "lukas-reineke/indent-blankline.nvim",
+        event = { "BufReadPost", "BufNewFile" },
+        main = "ibl",
+        opts = {
+            indent = {
+                char = "│",
+            },
+            exclude = {
+                filetypes = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm",
+                    "lazyterm",
+                },
+            },
+            scope = { enabled = false },
+        },
+    },
 
-  -- lsp symbol navigation for lualine. This shows where
-  -- in the code structure you are - within functions, classes,
-  -- etc - in the statusline.
-  {
-    "SmiteshP/nvim-navic",
-    lazy = true,
-    init = function()
-      vim.g.navic_silence = true
-      require("custom.util").lsp.on_attach(function(client, buffer)
-        if client.server_capabilities.documentSymbolProvider then
-          require("nvim-navic").attach(client, buffer)
-        end
-      end)
-    end,
-    opts = function()
-      return {
-        separator = " ",
-        highlight = true,
-        depth_limit = 5,
-        icons = require("custom.util.icons").kinds
-      }
-    end,
-  },
-  { "nvim-tree/nvim-web-devicons", lazy = true }, -- icons
-  { "MunifTanjim/nui.nvim",        lazy = true }, -- ui components
+    { "folke/which-key.nvim", opts = {} },
+
+    { "nvim-tree/nvim-web-devicons", lazy = true }, -- icons
+    { "MunifTanjim/nui.nvim", lazy = true }, -- ui components
+
+    { "Bekaboo/dropbar.nvim", event = "VeryLazy" },
 }
