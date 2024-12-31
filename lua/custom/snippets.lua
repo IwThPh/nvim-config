4583ad6d502e3c44bded2140660cab8052944657
Iwan Phillips <iwan@iwanphillips.dev>
2024-12-31 16:55:51 +0000

    refactor: snippets

--- lua/custom/snippets.lua
+++ lua/custom/snippets.lua
@@ -44,8 +44,7 @@ require("luasnip.loaders.from_vscode").lazy_load({
     paths = { "~/.config/nvim/snippets/php" },
 })
 
--- TODO: consider lua dir
-for _, ft_path in ipairs(vim.api.nvim_get_runtime_file("snippets/*.lua", true)) do
+for _, ft_path in ipairs(vim.api.nvim_get_runtime_file("lua/custom/snippets/*.lua", true)) do
     loadfile(ft_path)()
 end
 
